82f320cfee2124ed801e9ea423b7c6880b1eced7
黑枫叶 <454383818@qq.com>
2024-09-03 15:15:55 +0800

    update

--- src/helper/request.js
+++ src/helper/request.js
@@ -12,11 +12,19 @@ if (typeof (import.meta.env['VITE_API_URL']) == "string" && import.meta.env.VITE
 
 export const request = {
     format: (url) => {
-        return API_URL + "/" + url.replace(/^\/+/, '')
+        if (url.indexOf(API_URL) != 0) {
+            url = API_URL + "/" + url.replace(/^\/+/, '')
+        }
+        return url;
     },
-    send: async (method, url, values, is_json = true,options={}) => {
+    fetch: async (url, options = {}) => {
         NProgress.start();
         let furl = request.format(url);
+        let result = await fetch(furl, options)
+        NProgress.done();
+        return result;
+    },
+    send: async (method, url, values, is_json = true, options = {}) => {
         let fopts = {
             'method': method,
             'headers': {
@@ -31,22 +39,41 @@ export const request = {
         }
         fopts = Object.assign(fopts, options);
         try {
-            let response = await fetch(furl, fopts)
+            let response = await request.fetch(url, fopts)
             let json = await response.json();
-            if (!json.flag) {
-                util.msg.fail(json.msg)
-                if(typeof(json.code) == 'number' && json.code == 403){
+            if (json.code) {
+                util.msg.fail(json.message)
+                if (json.code == 403) {
                     auth.quit();
                     router.replace("/");
                 }
             }
             return json;
         } catch (err) {
-            let fail_msg = typeof (err.msg) != 'undefined' ? err.msg : "网络请求出错";
+            let fail_msg = typeof (err.message) != 'undefined' ? err.message : "网络请求出错";
             util.msg.fail(fail_msg);
-            return { flag: false, data: [], msg: fail_msg };
-        }finally{
-            NProgress.done();
+            return { code: 500, data: [], message: fail_msg };
+        }
+    },
+    download: async (method, url, values, options = {}) => {
+        let params = new URLSearchParams(values).toString();
+        let url1 = `${url}?${params}`
+        console.log(url1);
+        let fopts = {
+            'method': method,
+            'headers': {
+                'Auth-Token': auth.data.authtoken,
+            },
+        }
+        fopts = Object.assign(fopts, options);
+        try {
+            let response = await request.fetch(url1, fopts)
+            let data = await response.blob();
+            let blobUrl = window.URL.createObjectURL(data);
+            window.open(blobUrl, '_blank');
+        } catch (err) {
+            let fail_msg = typeof (err.message) != 'undefined' ? err.message : "网络请求出错";
+            util.msg.fail(fail_msg);
         }
     },
     get: async (url, values, is_json = true) => {
