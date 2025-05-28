de73881358a432eae4841e426edfee694592a02e
黑枫叶@home.pc <454383818@qq.com>
2025-05-28 22:27:19 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import "nprogress/nprogress.css";
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { router } from '@/router/router'
+import { router } from '@/router'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import CScrollbar from 'c-scrollbar';
 
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,3 +1,9 @@
 <template>
     <h1>Hello World</h1>
 </template>
+
+<script setup>
+import {router} from '@/router'
+import { request } from '@/helper'
+import * as stores from '@/stores'
+</script>
