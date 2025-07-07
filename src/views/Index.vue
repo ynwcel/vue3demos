b17c46eb8fddb2a4215460a854e62cdc89a350e2
黑枫叶@home.pc <454383818@qq.com>
2025-07-07 20:20:09 +0800

    add-tailwindcss

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Hello World</h1>
+    <h1 class="text-3xl font-bold underline text-yellow-500">Hello World</h1>
 </template>
 
 <script setup>
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         vue(),
         AutoImport({   // 注册并使用配置项
             imports: [
